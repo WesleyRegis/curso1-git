68815152931c2198f588c11d6df36b9b7127f023
WesleyRegis <wesley_pgx@hotmail.com>
2020-05-12 16:29:43 -0300

    Projeto iniciado, mostrando a mensagem Ola mundo

---
+++ projeto-teste1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
